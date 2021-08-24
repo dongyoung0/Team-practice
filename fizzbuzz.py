90099515864ac398ec42c448feda5581d10ae3ea
dongyoung0 <wmkimdy@gmail.com>
2021-08-24 17:00:50 +0900

    refactor: Fizzbuzz in 1 if statement
    Refactor fizzbuzz in 1 if statement from 3 if statement.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -5,11 +5,7 @@ n = int(input('input number: '))
 
 #print fizzbuzz
 for i in range(1,n+1):
-    if i%3==0 and i%5==0:
-        print(f'{i} fizzbuzz')
-    elif i%3==0:
-        print(f'{i} fizz')
-    elif i%5==0:
-        print(f'{i} buzz')
+    if i%3==0 or i%5==0:
+        print(f'{i} ' + 'fizz'*(i%3==0) + 'buzz'*(i%5==0))
     else:
         print(i)
